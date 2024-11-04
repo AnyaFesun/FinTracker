8b62e9b409e5091dce545461d1cd19a85d009016
Anna <anna.fesun825@gmail.com>
2024-11-04 15:46:01 +0200

    Created basic structure of project

---
+++ src/main/java/org/example/back_end_2/BackEnd2Application.java
@@ -0,0 +1,13 @@
+package org.example.back_end_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEnd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEnd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/back_end_2/BackEnd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.back_end_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEnd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
